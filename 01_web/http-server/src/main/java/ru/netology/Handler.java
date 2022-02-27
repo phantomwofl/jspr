73d891f4bee74cef80ab80d1afafb00f8eba4c83
phantomwofl <phantomwolf@yandex.ru>
2022-02-27 12:12:02 +0300

    origin master

--- 01_web/http-server/src/main/java/ru/netology/Handler.java
+++
@@ -1,7 +0,0 @@
-package ru.netology;
-
-import java.io.BufferedOutputStream;
-
-public interface Handler {
-    public void handle(Request request, BufferedOutputStream responseStream);
-}
--- 01_web/http-server/src/main/java/ru/netology/Main.java
+++ 01_web/http-server/src/main/java/ru/netology/Main.java
@@ -6,22 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final var server = new Server();
+        final var server = new Server(9999, 64);
         server.start();
-
-        server.addHandler("GET", "/messages", new Handler() {
-            @Override
-            public void handle(Request request, BufferedOutputStream responseStream) {
-                //todo
-            }
-        });
-
-        server.addHandler("POST", "/messages", new Handler() {
-            @Override
-            public void handle(Request request, BufferedOutputStream responseStream) {
-                //todo
-            }
-        });
-
         }
 }
